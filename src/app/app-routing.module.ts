941800e1de18a4e856f3bd399c4566e41072a16d
gioshwua <reanokennethjoshua11@gmail.com>
2022-11-11 10:23:22 +0800

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'landing',
+    loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule)
+  },
 ];
 
 @NgModule({
