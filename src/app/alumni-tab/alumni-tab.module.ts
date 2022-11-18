5bf4d6855d8df566f06fd6133809448d849dd5bc
gioshwua <reanokennethjoshua11@gmail.com>
2022-11-18 12:49:27 +0800

    added infinite scroll

--- src/app/alumni-tab/alumni-tab.module.ts
+++ src/app/alumni-tab/alumni-tab.module.ts
@@ -5,16 +5,11 @@ import { NavBarComponent } from '../Components/nav-bar/nav-bar.component';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from '../Components/header/header.component';
 import { AlumniTabPageRoutingModule } from './alumni-tab-routing.module';
-import { AlumniInfoComponent } from '../Components/alumni-info/alumni-info.component';
+
 import { AlumniTabPage } from './alumni-tab.page';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, AlumniTabPageRoutingModule],
-  declarations: [
-    AlumniTabPage,
-    NavBarComponent,
-    HeaderComponent,
-    AlumniInfoComponent,
-  ],
+  declarations: [AlumniTabPage, NavBarComponent, HeaderComponent],
 })
 export class AlumniTabPageModule {}
--- src/app/alumni-tab/alumni-tab.page.ts
+++ src/app/alumni-tab/alumni-tab.page.ts
@@ -6,10 +6,127 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alumni-tab.page.scss'],
 })
 export class AlumniTabPage implements OnInit {
+  page = 0;
+  perPage = 10;
+  array: any[] = [
+    {
+      id: 1,
+      image: 'pic ',
+      name: 'Joshua Reano',
+      address: 'dummy address',
+      department: '12344556',
+      course: 'BSCS',
+      stat: 'TAMBAY',
+    },
+    {
+      id: 2,
+      image: 'pic ',
+      name: 'Hubert Espinola',
+      address: 'dummy',
+      department: 'ffdfdfsdfdfsfrs',
+      course: 'BSCS',
+      stat: 'TAMBAY',
+    },
+    {
+      id: 3,
+      image: 'pic ',
+      name: 'Jello Santos',
+      address: 'dummy',
+      department: '545454545',
+      course: 'BSCS',
+      stat: 'TAMBAY',
+    },
+    {
+      id: 4,
+      image: 'pic ',
+      name: 'Joshua Reano',
+      address: 'dummy address',
+      department: '12344556',
+      course: 'BSCS',
+      stat: 'TAMBAY',
+    },
+    {
+      id: 5,
+      image: 'pic ',
+      name: 'Joshua Reano',
+      address: 'dummy address',
+      department: '12344556',
+      course: 'BSCS',
+      stat: 'TAMBAY',
+    },
+    {
+      id: 6,
+      image: 'pic ',
+      name: 'Joshua Reano',
+      address: 'dummy address',
+      department: '12344556',
+      course: 'BSCS',
+      stat: 'TAMBAY',
+    },
+    {
+      id: 7,
+      image: 'pic ',
+      name: 'Joshua Reano',
+      address: 'dummy address',
+      department: '12344556',
+      course: 'BSCS',
+      stat: 'TAMBAY',
+    },
+    {
+      id: 8,
+      image: 'pic ',
+      name: 'Joshua Reano',
+      address: 'dummy address',
+      department: '12344556',
+      course: 'BSCS',
+      stat: 'TAMBAY',
+    },
+    {
+      id: 9,
+      image: 'pic ',
+      name: 'Joshua Reano',
+      address: 'dummy address',
+      department: '12344556',
+      course: 'BSCS',
+      stat: 'TAMBAY',
+    },
+    {
+      id: 10,
+      name: 'Joshua Reano',
+      address: 'dummy address',
+      department: '12344556',
+      course: 'BSCS',
+      stat: 'TAMBAY',
+    },
+  ];
+  lists: any[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.lists = this.paginateArray();
   }
 
+  paginateArray() {
+    this.page++;
+    return this.array.filter(
+      (x) =>
+        x.id > this.page * this.perPage - this.perPage &&
+        x.id <= this.page * this.perPage
+    );
+  }
+
+  loadMore(event) {
+    console.log(event);
+    setTimeout(() => {
+      const array = this.paginateArray();
+      console.log('new data: ', array);
+      this.lists = this.lists.concat(array);
+      console.log('list data: ', this.lists);
+      event.target.complete();
+      if (array?.length < this.perPage) {
+        event.target.disabled = true;
+      }
+    }, 1000);
+  }
 }
