173d8310932a88d92e2393614d1b1e8c13e0feb1
gioshwua <reanokennethjoshua11@gmail.com>
2022-11-19 19:16:42 +0800

    added register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,6 +31,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
 ];
 
 @NgModule({
