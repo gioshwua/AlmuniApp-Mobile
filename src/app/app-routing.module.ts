da3a782f52d3170013cfb20e794ab980074dfe75
gioshwua <reanokennethjoshua11@gmail.com>
2022-11-19 17:53:49 +0800

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,6 +27,10 @@ const routes: Routes = [
     path: 'events',
     loadChildren: () => import('./events/events.module').then( m => m.EventsPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
