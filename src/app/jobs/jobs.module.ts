8fb8b193ce84175b199fee29737518ea5189c519
gioshwua <reanokennethjoshua11@gmail.com>
2022-11-23 08:15:25 +0800

    jobs

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -5,16 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { JobsPageRoutingModule } from './jobs-routing.module';
-
+import { HeaderComponent } from '../Components/header/header.component';
 import { JobsPage } from './jobs.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    JobsPageRoutingModule
-  ],
-  declarations: [JobsPage]
+  imports: [CommonModule, FormsModule, IonicModule, JobsPageRoutingModule],
+  declarations: [JobsPage, HeaderComponent],
 })
 export class JobsPageModule {}
