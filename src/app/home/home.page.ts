10d262cd1041bb11a91008afa75df78b7875c1e7
HueHueberry <huby.new1112@gmail.com>
2022-11-29 22:52:54 +0800

    Implemented login method Initializes design for backend flow

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RequestlibService } from '../Services/requestlib.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public username: any;
+  public password: any;
 
-  constructor() {}
+  constructor(public requestLib: RequestlibService) {
+    // will be changed depends on where the IP
+    // of the API server will be deployed.
+    this.requestLib.setURI('http://localhost:5050');
+  }
 
+  login() {
+    const credentails = {'username': this.username, 'password': this.requestLib.md5(this.password)};
+    const accepted = (response) => {
+      if (response['status'] == 200) {
+        this.requestLib.setCookieValue('token', response['data']['token']);
+        this.requestLib.initializeID(response['data']['id']);
+      }
+    };
+
+    this.requestLib.generateToken(credentails, accepted);
+  }
 }
