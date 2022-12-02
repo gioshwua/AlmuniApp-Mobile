a4c87c4f943b425dc143bcd94c50f7095a26f3fa
gioshwua <reanokennethjoshua11@gmail.com>
2022-12-02 23:28:47 +0800

    modified events TS

--- src/app/events/events.page.ts
+++ src/app/events/events.page.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.page.scss'],
 })
 export class EventsPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
